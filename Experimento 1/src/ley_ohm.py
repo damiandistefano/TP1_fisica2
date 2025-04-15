54a49c112d6b390c21196b2086e989af796f40e5
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2025-04-15 11:10:15 -0300

    ley de ohm ej1

--- Experimento 1/src/ley_ohm.py
+++ Experimento 1/src/ley_ohm.py
@@ -2,25 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-def graficar_ley_ohm(df, v_col, i_col):
+def graficar_ley_ohm(df1, df2, v_col, i_col):
     
-    V = df[v_col]
-    I = df[i_col]
+    V1 = df1[v_col]
+    I1 = df1[i_col]
+
+    V2 = df2[v_col]
+    I2 = df2[i_col]
 
     # Ajuste lineal
-    pendiente, intercepto, r_value, p_value, std_err = linregress(I, V)
+    pendiente1, intercepto1, r_value1, p_value1, std_err1 = linregress(I1, V1)
+    pendiente2, intercepto2, r_value2, p_value2, std_err2 = linregress(I2, V2)
 
     # Plot
     plt.figure(figsize=(8, 5))
-    plt.scatter(I, V, label='Datos experimentales', color='blue')
-    plt.plot(I, pendiente * I + intercepto, label=f'Ajuste lineal: V = {pendiente:.2f} I + {intercepto:.2f}', color='red', linestyle='--')
+    plt.scatter(I1, V1, color='black')
+    plt.plot(I1, pendiente1 * I1 + intercepto1, label='R = 9600 $\omega$', color='red', linestyle='--')
+    plt.scatter(I2, V2, color='black')
+    plt.plot(I2, pendiente2 * I2 + intercepto2, label='R = 98000 $\omega$', color='blue', linestyle='--')
     plt.xlabel("Corriente [A]")
     plt.ylabel("Voltaje [V]")
     plt.title(f"Ley de Ohm: {v_col} vs {i_col}")
     plt.grid(True)
     plt.legend()
+    plt.ylim(0, 5)
+
     plt.tight_layout()
     plt.show()
 
-    print(f"Pendiente (resistencia estimada): {pendiente:.2f} ohm")
-    print(f"R² del ajuste: {r_value**2:.4f}")
+    # print(f"Pendiente (resistencia estimada): {pendiente:.2f} ohm")
+    # print(f"R² del ajuste: {r_value**2:.4f}")
